826bfea42c23adad29870d27a09e2210ef8c7eed
Pierre-Yves Aillet <pyaillet@gmail.com>
2021-12-01 21:46:00 +0100

    day 1

---
+++ day_01_1/src/main.rs
@@ -0,0 +1,38 @@
+use itertools::Itertools;
+use std::fs;
+
+fn get_increases(input: &str) -> usize {
+    input
+        .lines()
+        .filter_map(|line| line.parse::<usize>().ok())
+        .tuple_windows::<(_, _)>()
+        .filter(|(a, b)| a < b)
+        .count()
+}
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    println!("{}", get_increases(&contents));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_increases() {
+        let input_test = "
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+";
+        assert_eq!(get_increases(input_test), 7);
+    }
+}
