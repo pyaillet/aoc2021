c18d37b6ccaecc5c2c1024d701339c11ee21c61a
Pierre-Yves Aillet <pyaillet@gmail.com>
2021-12-02 07:35:21 +0100

    day 2

---
+++ day_02_2/src/main.rs
@@ -0,0 +1,81 @@
+use std::fs;
+use std::str::FromStr;
+
+#[derive(Debug, Default)]
+struct Position {
+    aim: i32,
+    depth: i32,
+    forward: i32,
+}
+
+impl Position {
+    fn take_order(&mut self, order: Order) -> () {
+        match order {
+            Order::Forward(forward) => {
+                self.forward += forward;
+                self.depth += self.aim * forward;
+            }
+            Order::Down(down) => self.aim += down,
+            Order::Up(up) => self.aim -= up,
+        };
+    }
+}
+
+enum Order {
+    Forward(i32),
+    Up(i32),
+    Down(i32),
+}
+
+impl FromStr for Order {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
+        let mut order = s.split(" ");
+        let order_text = order.next().unwrap();
+        let order_value = order.next().unwrap().parse::<i32>().unwrap();
+        match order_text {
+            "forward" => Ok(Order::Forward(order_value)),
+            "up" => Ok(Order::Up(order_value)),
+            "down" => Ok(Order::Down(order_value)),
+            _ => Err(()),
+        }
+    }
+}
+
+fn parse_orders(input: &str) -> Vec<Order> {
+    input
+        .lines()
+        .map(|line| line.parse::<Order>().unwrap())
+        .collect()
+}
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    let orders = parse_orders(&contents);
+    let mut position = Position::default();
+    for order in orders {
+        position.take_order(order);
+    }
+    println!("{}", position.depth * position.forward);
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let input = "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2";
+        let orders = super::parse_orders(input);
+        let mut position = super::Position::default();
+        for order in orders {
+            position.take_order(order);
+        }
+        assert_eq!(position.depth * position.forward, 900);
+    }
+}
